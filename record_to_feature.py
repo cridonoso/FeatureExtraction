dc2e1dd7d6727756ed50ea4a4786d051084df46b
Cristobal <cridonoso@udec.cl>
2020-08-10 18:47:43 -0400

    css

--- record_to_feature.py
+++ record_to_feature.py
@@ -16,7 +16,7 @@ class_code = {'wise':{'NC':0, 'RRab':1, 'RRc':2, 'DSCT_SXPHE':3, 'CEP':4, 'SRV':
 			  'ogle':{'cep': 0, 'RRab': 1, 'RRc': 2, 'dsct': 3, 'EC': 4, 'ED': 5,  'ESD': 6, 'Mira': 7, 'SRV': 8, 'OSARG': 9, 'std': 10},
 			  'gaia':{'CEP': 0, 'T2CEP': 1, 'MIRA_SR': 2, 'DSCT_SXPHE':3, 'RRAB':4, 'RRC':5, 'RRD':6},
 			  # 'css':{'ACEP':0, 'Blazkho':1, 'CEPII':2, 'DSC':3, 'EA':4, 'EA_UP':5, 'ELL':6, 'EW':7, 'HADS':8, 'LPV':9, 'Misc':10, 'RRab':11, 'RRc':12, 'RRd':13, 'RS_CVn':14, 'Rotational Var':15, 'Transient':16, 'beta_Lyrae':17},
-			  'css':{'SN':0, 'CV':1,'AGN':2, 'HPM':3, 'Blazar':4 ,'Flare':5, 'Unknown':6},
+			  'css':{'RRab':0,'RRc':1, 'RRd':2,'beta_Lyrae':3, 'EA':4,'EA_UP':5, 'EW':6, 'ACEP':7,'CEPII':8, 'RS_CVn':9, 'DSC':10, 'Rotational Var':11, 'HADS':12, 'LPV':13, 'Misc':14, 'Blazkho':15, 'ELL':16,'Transient':17},			  
 			  'macho': {'QSO':0, 'Be':1, 'CEPH':2, 'RRL':3, 'EB':4, 'MOA':5, 'LPV':6},
 			  #'linear': {'RRLab':0, 'RRLc':1, 'Eclipsing Algol':2, 'Contact binary':3, 'DSCT':4},
 			  'linear': {'Beta_Persei':0, 'Delta_Scuti':1, 'RR_Lyrae_FM':2, 'RR_Lyrae_FO':3, 'W_Ursae_Maj':4},
